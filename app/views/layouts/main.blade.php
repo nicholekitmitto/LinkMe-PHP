58849171e739779ed233a3644450fe432a09d932
Nichole Dwight <nicholedwight@gmail.com>
2014-04-14 10:50:33 +0100

    Setting up layout

---
+++ app/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LinkMe</title>
+    {{ HTML::style('packages/foundation/css/foundation.min.css') }}
+    {{ HTML::style('css/main.css')}}
+  </head>
+
+  <body>
+
+  </body>
+</html>
