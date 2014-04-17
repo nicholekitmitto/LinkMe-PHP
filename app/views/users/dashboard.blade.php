d6690d5113b9d832483b8b64db54b85cd2a25026
Nichole Dwight <nicholedwight@gmail.com>
2014-04-17 21:41:03 +0200

    Date formats for links

--- app/views/users/dashboard.blade.php
+++ app/views/users/dashboard.blade.php
@@ -2,18 +2,19 @@
 <p>Welcome to your Dashboard. You're awesome!</p>
 <div class="dashboard-options">
   <ul>
-    <a href="show">Your Profile</a>
-    <a href="#">Your links</a>
-    <a href="/users/index">Users</a>
+    <li><a href="show">Your Profile</a></li>
+    <li><a href="#">Viewed links</a></li>
+    <li><a href="/users/index">All Users</a></li>
   </ul>
 </div>
 <div class="your-questions">
 
   @foreach ($links as $link)
-    <p>
+    <p class="individual-link">
+      <span class="sent-on">{{date("F j, Y, g:i a", strtotime($link->created_at))}}</span>
       <span class="message">{{$link->message}}</span>
-      <span class="link">{{$link->link}}</span>
-      <span class="sent-from">Sent from: {{User::getFullNameFromId($link->sender_id);}}</span>
+      <span class="link"><a href="{{$link->link}}">{{$link->link}}</a></span>
+      <span class="sent-from">From: {{User::getFullNameFromId($link->sender_id);}}</span>
       </p>
   @endforeach
 
--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,5 +1,10 @@
-@foreach ($users as $user)
-  <a href="{{$user->id}}/show" class="usernames">
-    {{$user->firstname}} {{$user->lastname}}
-  </a>
-@endforeach
+<h3>Send someone a link</h3>
+<ul class="users-list">
+  @foreach ($users as $user)
+    <li>
+      <a href="{{$user->id}}/show" class="usernames">
+      {{$user->firstname}} {{$user->lastname}}
+      </a>
+    </li>
+  @endforeach
+</ul>
