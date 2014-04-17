84f38bc8367b3f769e83d72d200c6446900e1c2e
Nichole Dwight <nicholedwight@gmail.com>
2014-04-17 22:02:27 +0200

    Send a link form does not show for own profile page

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,10 +2,12 @@
 {{$user->firstname}} {{$user->lastname}}
 </h1>
 
-
-{{ Form::open(array('url'=>"users/$user->id/links/create", 'class'=>'form-signup')) }}
-{{ Form::textarea('message', null, array('class'=>'input-block-level', 'placeholder'=>'Enter a message')) }}
-{{ Form::text('link', null, array('class'=>'input-block-level', 'placeholder'=>'Enter the link here')) }}
-{{ Form::hidden('recipient_id', $user->id)}}
-{{ Form::submit('Submit', array('class'=>'small round button'))}}
-{{ Form::close() }}
+@if ($user->id != Auth::user()->id)
+  {{ Form::open(array('url'=>"users/$user->id/links/create", 'class'=>'form-signup')) }}
+  {{ Form::textarea('message', null, array('class'=>'input-block-level', 'placeholder'=>'Enter a message')) }}
+  {{ Form::text('link', null, array('class'=>'input-block-level', 'placeholder'=>'Enter the link here')) }}
+  {{ Form::hidden('recipient_id', $user->id)}}
+  {{ Form::submit('Submit', array('class'=>'small round button'))}}
+  {{ Form::close() }}
+@else {{"HEY"}}
+@endif
