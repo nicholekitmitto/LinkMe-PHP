18079c693fc972fc4e9ed72c1377c4049753a57f
Nichole Dwight <nicholedwight@gmail.com>
2014-04-25 15:48:19 +0100

    Removed error outputs from individual forms, now in the main layout

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -1,11 +1,7 @@
 {{ Form::open(array('url'=>'users/create', 'class'=>'form-signup')) }}
     <h2 class="form-signup-heading">Please Register</h2>
 
-    <ul>
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
+  
 
     {{ Form::text('firstname', null, array('class'=>'input-block-level', 'placeholder'=>'First Name')) }}
     {{ Form::text('lastname', null, array('class'=>'input-block-level', 'placeholder'=>'Last Name')) }}
