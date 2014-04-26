09cf6a265add3a5c32935f6c45af2263d84a1fb1
Nichole Dwight <nicholedwight@gmail.com>
2014-04-26 12:44:12 +0100

    Added filtering for home/viewed links pages

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -3,6 +3,7 @@ class UsersController extends BaseController {
   public function __construct() {
     $this->beforeFilter('csrf', array('on'=>'post'));
     $this->beforeFilter('auth', array('only'=>array('getDashboard')));
+    $this->beforeFilter('isOwnUser', array('only'=>array('getDashboard', 'getDashboardViewed')));
   }
 
   protected $layout = "layouts.main";
