d11c0d6ca99bc354ecee7f482d3ac8890d89853e
Nichole Dwight <nicholedwight@gmail.com>
2014-04-26 12:45:38 +0100

    Rearranged link submission form, and added in character maximum for message

--- app/views/users/dashboardviewed.blade.php
+++ app/views/users/dashboardviewed.blade.php
@@ -1,14 +1,15 @@
 <h1>Old Links</h1>
 <p>These are your links that have been marked as viewed.</p>
 
-<div class="your-questions">
-
-@foreach ($oldLinks as $link)
-  <p class="individual-link">
-    <span class="sent-on">{{date("F j, Y, g:i a", strtotime($link->created_at))}}</span>
-    <span class="message">{{$link->message}}</span>
-    <span class="link"><a href="{{$link->link}}">{{$link->link}}</a></span>
-    <span class="sent-from">From: {{User::getFullNameFromId($link->sender_id);}}</span>
-    </p>
-@endforeach
+<div class="row">
+  <ul class="link-rows">
+    @foreach ($oldLinks as $link)
+      <li class="individual-link">
+        <span class="sent-on">{{date("F j, Y, g:i a", strtotime($link->created_at))}}</span>
+        <span class="message">{{$link->message}}</span>
+        <span class="link"><a href="{{$link->link}}">{{$link->link}}</a></span>
+        <span class="sent-from">From: {{User::getFullNameFromId($link->sender_id);}}</span>
+      </li>
+    @endforeach
+  </ul>
 </div>
