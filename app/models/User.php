fbb3116416a22231ccf58f488ac95e1b2633cdfc
Nichole Dwight <nicholedwight@gmail.com>
2014-04-26 14:57:51 +0100

    migrated remember_tokens

--- app/models/User.php
+++ app/models/User.php
@@ -56,6 +56,21 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		return $this->email;
 	}
 
+	public function getRememberToken()
+	{
+	    return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
+	}
+
 	public function Links()
 		{
 				return $this->hasMany('links');
